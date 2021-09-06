5210025ee3d027070b6cd1f8676c50e90656a9e4
Rafael Andara <rafael.andara@admios-sa.com>
2021-09-05 21:46:44 -0300

    User model, validations for strong password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,42 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :name, presence: true
-  validates :password_digest, presence: true
+  validates :password, presence: true
+
+  validates :password, length: { in: 10..16 }
+
+  validate :at_least_one_lowercase_character_in_password
+  validate :at_least_one_uppercase_character_in_password
+  validate :at_least_one_digit_in_password
+  validate :no_three_repeating_characters_in_a_row_in_password
+
+  private
+
+  def at_least_one_lowercase_character_in_password
+    if !/[[:lower:]]/.match?(password)
+      errors.add(:password, "doesn't contains a lowercase char")
+    end
+  end
+
+  def at_least_one_uppercase_character_in_password
+    if !/[[:upper:]]/.match?(password)
+      errors.add(:password, "doesn't contains an uppercase char")
+    end
+  end
+
+  def at_least_one_digit_in_password
+    if !/[[:digit:]]/.match?(password)
+      errors.add(:password, "doesn't contains a digit")
+    end
+  end
+
+  def no_three_repeating_characters_in_a_row_in_password
+    return false if password.nil?
+    repeated_chars = password.scan(/((.)\2{2,})/).map(&:last)
+    if !repeated_chars.empty?
+      repeated_chars.each do |char|
+        errors.add(:password, "has '#{char}' character repeated more than 3 times in a row")
+      end
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,23 +1,143 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'Validations' do
-    it 'should fail to create if name is not present' do
-      expect{ 
-        User.create!(password_digest: 'any_password')
-      }.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Name can't be blank")
+  describe 'valid?' do
+    before(:each) do
+      @user = User.new(name: 'username')
     end
 
-    it 'should fail to create if password_digest is not present' do
-      expect{ 
-        User.create!(name: 'any_name')
-      }.to raise_error(ActiveRecord::RecordInvalid, "Validation failed: Password can't be blank, Password digest can't be blank")
+    it 'should not be valid without name and password' do
+      @user.name = nil
+      expect(@user.valid?).to be false
     end
 
-    it 'should create a user if required attributes are present' do
-      expect{ 
-        User.create!(name: 'any_name', password_digest: 'any_password')
-      }.to change{ User.count }.by(1)
+    it 'should not be valid without password' do
+      expect(@user.valid?).to be false
+    end
+
+    it 'should not be valid without name' do
+      @user.name = nil
+      @user.password = '1Password123'
+      expect(@user.valid?).to be false
+    end
+
+    it 'should not be valid if password length is less than 10' do
+      @user.password = '1'
+      expect(@user.valid?).to be false
+    end
+
+    it 'should not be valid if password length is greater that 16' do
+      @user.password = 'ABCDEFGHIJKLMNOPQR'
+      expect(@user.valid?).to be false
+    end
+
+    it 'should not be valid without a lowercase char in password' do
+      @user.password = '1PASSWORD123'
+      expect(@user.valid?).to be false
+    end
+
+    it 'should not be valid without an uppercase char in password' do
+      @user.password = '1password123'
+      expect(@user.valid?).to be false
+    end
+
+    it 'should not be valid without a digit in password' do
+      @user.password = 'PPasswordXYZ'
+      expect(@user.valid?).to be false
+    end
+
+    it 'should not be valid with three or more repeating characters in password' do
+      @user.password = '1Passsword123'
+      expect(@user.valid?).to be false
+    end
+
+    it 'should be valid with name and password' do
+      @user.password = '1Password123'
+      expect(@user.valid?).to be true
+    end
+  end
+
+  describe 'errors size' do
+    before(:each) do
+      @user = User.new(name: 'username')
+    end
+
+    it 'should not have any errors for "QPFJWz1343439"' do
+      @user.password = 'QPFJWz1343439'
+      @user.valid?
+      expect(@user.errors).to be_empty
+    end
+
+    it 'should have 1 error for "AAAfk1swods"' do
+      @user.password = 'AAAfk1swods'
+      @user.valid?
+      expect(@user.errors.size).to eq 1
+    end
+
+    it 'should have 1 error "Abc123"' do
+      @user.password = 'Abc123'
+      @user.valid?
+      expect(@user.errors.size).to eq 1
+    end
+
+    it 'should have 5 errors "000aaaBBBccccDDD"' do
+      @user.password = '000aaaBBBccccDDD'
+      @user.valid?
+      expect(@user.errors.size).to eq 5
+    end
+  end
+
+  describe 'custom validation methods' do
+    before(:each) do
+      @user = User.new(name: 'username')
+    end
+
+    describe 'at_least_one_lowercase_character_in_password' do
+      it 'should return error if password does not have a lowercase char' do
+        @user.password = "A"
+        expect(@user.send(:at_least_one_lowercase_character_in_password)).to_not be_nil
+      end
+
+      it 'should return nil if password does have a lowercase char' do
+        @user.password = "a"
+        expect(@user.send(:at_least_one_lowercase_character_in_password)).to be_nil
+      end
+    end
+
+    describe 'at_least_one_uppercase_character_in_password' do
+      it 'should return error if password does not have a uppercase char' do
+        @user.password = "a"
+        expect(@user.send(:at_least_one_uppercase_character_in_password)).to_not be_nil
+      end
+
+      it 'should return nil if password does have a uppercase char' do
+        @user.password = "A"
+        expect(@user.send(:at_least_one_uppercase_character_in_password)).to be_nil
+      end
+    end
+
+    describe 'at_least_one_digit_in_password' do
+      it 'should return error if password does not have a digit' do
+        @user.password = "Aa"
+        expect(@user.send(:at_least_one_digit_in_password)).to_not be_nil
+      end
+
+      it 'should return nil if password does have a digit' do
+        @user.password = "1Aa"
+        expect(@user.send(:at_least_one_digit_in_password)).to be_nil
+      end
+    end
+
+    describe 'no_three_repeating_characters_in_a_row_in_password' do
+      it 'should return repeatings char if password has three repeating characters in a row' do
+        @user.password = "123aaaABC"
+        expect(@user.send(:no_three_repeating_characters_in_a_row_in_password)).to eq ["a"]
+      end
+
+      it 'should return nil if password has not a repeating characters in a row' do
+        @user.password = "123abcABC"
+        expect(@user.send(:no_three_repeating_characters_in_a_row_in_password)).to be_nil
+      end
     end
   end
 end
